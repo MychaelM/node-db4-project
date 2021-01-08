b47bdae162cd8d0169abcfa776a8867734588471
Mychael M <mychaelbmenges@gmail.com>
2021-01-07 17:39:59 -0700

    created migration and added first table

---
+++ data/migrations/20210107173326_initial.js
@@ -0,0 +1,11 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("recipes", table => {
+    table.increments("id")
+    table.text("name").notNull().unique()
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("recipes")
+};
